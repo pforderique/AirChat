43a62be886b4d7f4c8d1d68fff9a01231d1c0a78
pforderique <porderiq@mit.edu>
2021-06-02 14:49:30 -0400

    registration validators working

--- application.py
+++ application.py
@@ -1,11 +1,17 @@
 from  flask import Flask, render_template
+from wtform_fields import *
 
 app = Flask(__name__)
 app.secret_key = 'replace later'
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
-    return render_template('index.html')
+    reg_form = RegistrationForm() # custom form we created
+
+    #* will return TRUE if VALID form was submitted using the POST method
+    if reg_form.validate_on_submit():
+        return "success!!"
+    return render_template('index.html', form=reg_form)
 
 if __name__ == "__main__":
     app.run(debug=True)
