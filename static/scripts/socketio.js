5226c8f39b6fdb1c6f9be421a64619b36b4ef31c
pforderique <porderiq@mit.edu>
2021-06-03 17:56:57 -0400

    trying socketio loading on clinet side differntly

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var socket = io.connect('http://' + document.domain + ':' + location.port);
+    var socket = io.connect('/');//'http://' + document.domain + ':' + location.port);
     
     let room = "Lounge"; // auto join this room
     joinRoom(room)
