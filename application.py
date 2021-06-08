165106e6f95ba53473596d1407d39c5da3a7a215
pforderique <porderiq@mit.edu>
2021-06-08 16:44:46 -0400

    heroku commit migrated to postgresql on heroku

--- application.py
+++ application.py
@@ -3,5 +3,5 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()  #! use app.run() when deploying to heroku
-    # socketio.run(app, debug=True)
+    # app.run()  #! use app.run() when deploying to heroku
+    socketio.run(app, debug=True)
